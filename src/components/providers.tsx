5f84e49cb426647d0a08b9b4b742b825ec475ce0
Afolabi Abdulsamad <fullendmaestro@gmail.com>
2026-01-09 06:13:04 +0100

    Update vite as bundler

---
+++ src/components/providers.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { WagmiProvider, createConfig, http } from 'wagmi';
+import { sepolia } from 'wagmi/chains';
+import { injected } from 'wagmi/connectors';
+import { ClickProvider } from '@make-software/csprclick-ui';
+import { CsprClickInitOptions, CONTENT_MODE } from '@make-software/csprclick-core-types';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+export const wagmiConfig = createConfig({
+  chains: [sepolia],
+  connectors: [injected()],
+  transports: {
+    [sepolia.id]: http()
+  },
+  ssr: true
+});
+
+const clickOptions: CsprClickInitOptions = {
+  appName: 'CSPR.app',
+  contentMode: CONTENT_MODE.IFRAME,
+  providers: ['casper-wallet', 'ledger', 'metamask-snap'],
+  appId: 'csprclick-template'
+};
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <ClickProvider options={clickOptions}>{children}</ClickProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
+
+export default Providers;
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+import Providers from './components/providers';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <Providers>
+      <App />
+    </Providers>
+  </React.StrictMode>
+);
